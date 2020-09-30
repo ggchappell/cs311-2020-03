b05485a0c51c1718ae2efdaf1ad6ba61d80669eb
Glenn G. Chappell <ggchappell@alaska.edu>
2020-09-30 14:11:48 -0800

    merge_sort.cpp: FINISH - write function mergeSort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,4 +1,4 @@
-// merge_sort.cpp  UNFINISHED
+// merge_sort.cpp
 // Glenn G. Chappell
 // 2020-09-30
 //
@@ -24,6 +24,7 @@ using std::move;
 using std::begin;
 using std::end;
 using std::distance;
+using std::advance;
 using std::iterator_traits;
 #include <chrono>
 // Everything from <chrono> is preceded by std::
@@ -95,7 +96,25 @@ void stableMerge(FDIter first, FDIter middle, FDIter last)
 template <typename FDIter>
 void mergeSort(FDIter first, FDIter last)
 {
-    // WRITE THIS!!!
+    // Compute size of range
+    auto size = distance(first, last);
+
+    // BASE CASE
+    if (size <= 1)
+        return;
+
+    // RECURSIVE CASE
+
+    // Create iterator ("middle") to middle of range
+    auto middle = first;
+    advance(middle, size/2);
+
+    // Recursively sort the two lists
+    mergeSort(first, middle);
+    mergeSort(middle, last);
+
+    // And merge them
+    stableMerge(first, middle, last);
 }
 
 
